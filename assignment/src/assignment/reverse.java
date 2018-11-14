4d0e6d1288a0a347e31e1cc53cbc69c4de04963d
swetar1 <44639532+swetar1@users.noreply.github.com>
2018-11-14 18:06:18 +0530

    Add files via upload

---
+++ assignment/src/assignment/reverse.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public class reverse {
+
+	public static void main(String[] args) {
+		for(int i=10;i>0;i--)
+		{
+			System.out.println("reverse numbers are"+i);
+			
+			
+		}
+
+	}
+
+}
