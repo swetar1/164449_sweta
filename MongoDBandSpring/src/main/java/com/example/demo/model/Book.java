93159f76bdabc25da898d690122c3c7eee1b65b0
swetar1 <44639532+swetar1@users.noreply.github.com>
2019-02-14 16:18:55 +0530

    Add files via upload

---
+++ MongoDBandSpring/src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,39 @@
+package com.example.demo.model;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="urvi")
+public class Book {
+	
+	@Id
+	private int id;
+	
+	private String bookName;
+	private String authorName;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	@Override
+	public String toString() {
+		return "Book [Id=" + id + ", bookName=" + bookName + ", authorName=" + authorName + "]";
+	}
+
+}
